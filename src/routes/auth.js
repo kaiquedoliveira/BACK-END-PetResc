c2eab70e7ea82a53ca028ffc7028473515c3e902
kaiquedoliveira <kdeoliveiramacedo@gmail.com>
2025-09-30 16:34:39 -0300

    atualizando auth.js para expor o token do usuario no login do postman

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
 const { login, register, me } = require("../controller/authController");
 const { authenticateToken } = require("../middlewares/authMiddleware");
 
-
-
-// Login aberta
-router.post("/login", (req, res) => {
-  
-  res.json({ message: "Login realizado com sucesso!" });
-});
-
-// Registro aberta
-router.post("/register", (req, res) => {
-  res.json({ message: "Usuário registrado com sucesso!" });
-});
-
-module.exports = router;               
-router.get("/me", authenticateToken, me);     
-
+router.post("/login", login);           // Rota de login agora chama a função 'login'
+router.post("/register", register);     
+router.get("/me", authenticateToken, me); 
 module.exports = router;
