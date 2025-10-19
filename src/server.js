45ce25b6376398df1f78fb54b6fccb95a32205d8
kaique <kdeoliveiramacedo@gmail.com>
2025-10-19 01:15:44 -0300

    limpando o server.js e aplicando as alterções nas chamadas das rotas

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 // Import de rotas
 const authRoutes = require("./routes/auth");
 const usuariosRoutes = require("./routes/usuarios");
@@ -15,22 +18,14 @@ const relatoriosRoutes = require("./routes/relatorios");
 const feedRoutes = require("./routes/feed");
 const adocoesRoutes = require("./routes/adocoes");
 
-const adminRoutes = require("./routes/protectedRoutes");
 
-
-
-app.use(cors());
-app.use(express.json());
-
-// rotas admin
-app.use("/api", adminRoutes);
 
 // Rotas principais
 app.use("/auth", authRoutes);
-app.use("/usuarios", usuariosRoutes);-
+app.use("/usuarios", usuariosRoutes);
 app.use("/animais", animaisRoutes);
 app.use("/doacoes", doacoesRoutes);
-app.use("/larTemporario", larTemporarioRoutes);
+app.use("/lares-Temporarios", larTemporarioRoutes);
 app.use("/ongs", ongsRoutes);
 app.use("/relatorios", relatoriosRoutes);
 app.use("/feed", feedRoutes);
@@ -39,29 +34,30 @@ app.get("/api", (req, res) => {
   res.json({ message: "API rodando corretamente!" });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000 ");
 
-  app.use((err, req, res, next) => {
+app.get("/", (req, res) => {
+  res.json({ message: "API PetResc rodando corretamente!" });
+});
+
+app.use((err, req, res, next) => {
   console.error("UM ERRO OCORREU:", err.stack);
-
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-
-  res.status(statusCode).json({
-    error: "Ocorreu um erro no servidor. Tente novamente."
+  res.status(500).json({
+    error: "Ocorreu um erro inesperado no servidor. Tente novamente."
   });
 });
 
-  async function testConnection() {
-    try {
-      await prisma.$connect();
-      console.log("Conexão com o banco de dados OK! ");
-    } catch (err) {
-      console.error("Erro ao conectar no banco :", err);
-    } finally {
-      await prisma.$disconnect();
-    }
-  }
 
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+
+  console.log(`Servidor rodando na porta ${PORT}`);
+
+  console.log("Conexão com o banco de dados OK!");
+
+  
+
+  
   testConnection();
 });
