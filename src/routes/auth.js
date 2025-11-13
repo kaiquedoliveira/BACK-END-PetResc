06130a4dac687f6458759aee1bb7e7102bb4bec2
kaiquedoliveira <kdeoliveiramacedo@gmail.com>
2025-11-13 16:53:50 -0300

    finalizando o dia e incluindo funçaõ de atualizar perfil

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { authenticateToken } = require("../middlewares/authMiddleware");
-const { register, registerOng, login, me, forgotPassword, resetPassword } = require('../controller/authController');
+const { register, registerOng, login, me, forgotPassword, resetPassword, atualizarPerfil } = require('../controller/authController');
 
 
 router.post('/register', register);
 router.post('/register-ong', registerOng);
 router.post('/login', login);
 router.get('/me', authenticateToken, me);
-
+router.put('/me', authenticateToken, atualizarPerfil);
 router.post('/forgot-password', forgotPassword);
 
 router.post('/reset-password', resetPassword);
