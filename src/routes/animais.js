f1354189f93403cb02ed5430c966793f1fdec5c2
kaique <kdeoliveiramacedo@gmail.com>
2025-11-24 21:11:20 -0300

    Correcao rotas animais e permissao publico

--- src/routes/animais.js
+++ src/routes/animais.js
@@ -4,25 +4,25 @@ const animaisController = require('../controller/animaisController');
 const { authenticateToken, authorizeRole } = require('../middlewares/authMiddleware'); 
 const multer = require('multer');
 
-const storage = require('../config/cloudnary'); 
+const storage = require('../config/cloudinary'); 
 const upload = multer({ storage: storage });
 
 router.get('/', animaisController.listarAnimais);
 router.get('/:id', animaisController.buscarAnimalPorId);
 
-
 router.use(authenticateToken); 
 
-//  (PUBLICO e ONG )
 router.post('/:id/favoritar', animaisController.favoritarAnimal); 
 router.delete('/:id/desfavoritar', animaisController.desfavoritarAnimal);
 
-//  (ONG/ADMIN)
-router.post('/', authorizeRole('ONG', 'PUBLICO'), upload.single('imagem'), animaisController.criarAnimal); 
-router.put('/:id', authorizeRole(['ADMIN', 'ONG']), animaisController.atualizarAnimal);
-router.delete('/:id', authorizeRole(['ADMIN', 'ONG']), animaisController.deletarAnimal);
 
-router.get('/gerenciar/lista', authorizeRole(['ADMIN', 'ONG']), animaisController.listarAnimaisParaGerenciamento);
+router.post('/', authorizeRole(['ONG', 'PUBLICO']), upload.single('imagem'), animaisController.criarAnimal); 
+
+router.put('/:id', authorizeRole(['ADMIN', 'ONG', 'PUBLICO']), animaisController.atualizarAnimal);
+router.delete('/:id', authorizeRole(['ADMIN', 'ONG', 'PUBLICO']), animaisController.deletarAnimal);
+
+
+router.get('/gerenciar/lista', authorizeRole(['ADMIN', 'ONG', 'PUBLICO']), animaisController.listarAnimaisParaGerenciamento);
 router.get('/gerenciar/estatisticas/status', authorizeRole('ONG'), animaisController.obterEstatisticasAnimaisPorStatus); 
 
 module.exports = router;
