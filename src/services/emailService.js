208a88d92f781b6ea7cf2f6dbc1fbfcf8804cc9e
kaique <kdeoliveiramacedo@gmail.com>
2025-11-30 00:47:07 -0300

    implementando mudanças em no serviço de emails  para conseguir integrar o front corretamkenet

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,33 +2,31 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT), 
-  secure: false, 
+  port: Number(process.env.MAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false, 
-    ciphers: "SSLv3"           
-  },
-  connectionTimeout: 10000, 
+  }
 });
 
 const sendEmail = async (to, subject, html) => {
     try {
         console.log(`Tentando enviar e-mail para: ${to}...`);
+
         const info = await transporter.sendMail({
             from: `"PetResc" <${process.env.MAIL_USER}>`,
             to: to,
             subject: subject,
             html: html,
         });
-        console.log(` E-mail enviado: ${info.messageId}`);
+
+        console.log("E-mail enviado:", info.messageId);
         return true;
+
     } catch (error) {
-        console.error(`Falha no envio de e-mail: ${error.message}`);
-        return false; 
+        console.error("Falha no envio de e-mail:", error);
+        return false;
     }
 };
 
