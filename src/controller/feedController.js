04484c4c728807bee059aa9e6eafd591e7b3e18f
kaique <kdeoliveiramacedo@gmail.com>
2025-12-10 23:50:09 -0300

    enviando alterações

--- src/controller/feedController.js
+++ src/controller/feedController.js
@@ -1,43 +1,44 @@
-const { PrismaClient, StatusAdocao } = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getFeed = async (req, res) => {
   const { especie, porte, status, sexo } = req.query;
 
   try {
-    const animais = await prisma.animal.findMany({
-      where: {
-        AND: [
-          especie ? { especie: { contains: especie, mode: 'insensitive' } } : {},
-          porte ? { porte: { contains: porte, mode: 'insensitive' } } : {},
-          
-          
-          status 
-            ? { status: { equals: status } } 
-            : { status: { in: ['DISPONIVEL', 'ENCONTRADO','PERDIDO'] } },
+    const filtros = {
+      AND: [
+        especie ? { especie: { contains: especie, mode: 'insensitive' } } : {},
+        porte ? { porte: { contains: porte, mode: 'insensitive' } } : {},
+        sexo ? { sexo: { equals: sexo } } : {},
 
-          sexo ? { sexo: { equals: sexo } } : {}
-        ]
-      },
+        // 🔥 REGRA FINAL DO FEED 🔥
+        status
+          ? { status: { equals: status.toUpperCase() } }
+          : { status: { in: ['DISPONIVEL', 'PERDIDO', 'ENCONTRADO'] } }
+      ]
+    };
+
+    const animais = await prisma.animal.findMany({
+      where: filtros,
       include: {
         account: {
-            select: { 
-                id: true,
-                nome: true,
-                email: true,
-                telefone: true
-            }
+          select: {
+            id: true,
+            nome: true,
+            email: true,
+            telefone: true
+          }
         }
       },
-      orderBy: { createdAt: 'desc' } 
+      orderBy: { createdAt: "desc" }
     });
+
     res.json(animais);
+
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Erro ao buscar animais' });
+    res.status(500).json({ error: "Erro ao buscar animais" });
   }
 };
 
-module.exports = {
-  getFeed,
-};
+module.exports = { getFeed };
